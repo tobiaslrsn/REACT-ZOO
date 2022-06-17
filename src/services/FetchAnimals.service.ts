68a983ff326b6746d054efe474af6297997142e7
tobiaslrsn <nestorxxv@gmail.com>
2022-06-17 16:24:58 +0200

    .

--- src/services/FetchAnimals.service.ts
+++ src/services/FetchAnimals.service.ts
@@ -3,7 +3,8 @@ import { IAnimal } from "../models/IAnimal";
 
 export async function getAnimal(): Promise<IAnimal[]> {
   let response = await axios.get<IAnimal[]>(
-    `${process.env.REACT_APP_GET_ANIMALS}`
+    // `${process.env.REACT_APP_GET_ANIMALS}`
+    "https://animals.azurewebsites.net/api/animals"
   );
 
   console.log(response.data);
